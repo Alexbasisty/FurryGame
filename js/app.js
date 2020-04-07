a353d7dfb01bcba297495d84a409108c6cb79554
Alex Basystyi <alexbasisty@gmail.com>
2020-04-07 13:40:15 +0200

    first steps

--- js/app.js
+++ js/app.js
@@ -1,19 +1,41 @@
 console.log('hello');
 
-const Furry = () => {
-  this.x = 0;
-  this.y = 0;
-  this.direction = "right";
-};
+class Furry {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.direction = "right";
+  }
+}
 
-const Coin = () => {
-  this.x = Math.floor(Math.random() * 10);
-  this.y = Math.floor(Math.random() * 10);
-};
+class Coin {
+  constructor() {
+    this.x = Math.floor(Math.random() * 10);
+    this.y = Math.floor(Math.random() * 10);
+  }
+}
 
-const Game = () => {
+class Game  {
+  constructor() {
     this.board = document.querySelectorAll('#board div');
     this.furry = new Furry();
     this.coin = new Coin();
     this.score = 0;
-};
+  }
+
+    index = (x, y) => {
+     return x + (y * 10)
+  };
+
+    showFurry = () => {
+      this.board[this.index(this.furry.x, this.furry.y)].classList.add('furry');
+    };
+
+    showCoin = () => {
+      this.board[this.index(this.coin.x, this.coin.y)].classList.add('coin');
+    }
+}
+
+let startGame = new Game;
+startGame.showFurry();
+startGame.showCoin();
