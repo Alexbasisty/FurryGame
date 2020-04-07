0dc1f9a3fe11e108fc2064d74d46d4edf7c6cd87
Alex Basystyi <alexbasisty@gmail.com>
2020-04-07 12:50:30 +0200

    creating constructors

--- js/app.js
+++ js/app.js
@@ -10,3 +10,10 @@ const Coin = () => {
   this.x = Math.floor(Math.random() * 10);
   this.y = Math.floor(Math.random() * 10);
 };
+
+const Game = () => {
+    this.board = document.querySelectorAll('#board div');
+    this.furry = new Furry();
+    this.coin = new Coin();
+    this.score = 0;
+};
