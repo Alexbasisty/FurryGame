4703f921553e6064c84ca3b9f7ddbdd0e18752c1
Alex Basystyi <alexbasisty@gmail.com>
2020-04-08 11:23:47 +0200

    event listner to change directions

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ class Coin {
   }
 }
 
-class Game  {
+class Game {
   constructor() {
     this.board = document.querySelectorAll('#board div');
     this.furry = new Furry;
@@ -23,19 +23,62 @@ class Game  {
     this.score = 0;
   }
 
-    index = (x, y) => {
-     return x + (y * 10)
+  index = (x, y) => {
+    return x + (y * 10)
   };
 
-    showFurry = () => {
-      this.board[this.index(this.furry.x, this.furry.y)].classList.add('furry');
-    };
+  hideVisibleFurry = () => {
+    document.querySelector('.furry').classList.remove('furry');
 
-    showCoin = () => {
-      this.board[this.index(this.coin.x, this.coin.y)].classList.add('coin');
+  }
+
+  showFurry = () => {
+    this.board[this.index(this.furry.x, this.furry.y)].classList.add('furry');
+  };
+
+  showCoin = () => {
+    this.board[this.index(this.coin.x, this.coin.y)].classList.add('coin');
+  }
+
+  startGame = () => {
+    this.idSetinterval = setInterval(() => {
+      if (this.furry.direction === "right") {
+        this.furry.x = this.furry.x + 1;
+      } else if (this.furry.direction === "left") {
+        this.furry.x = this.furry.x - 1;
+      } else if (this.furry.direction === "down") {
+        this.furry.y = this.furry.y + 1;
+      } else if (this.furry.direction === "top") {
+        this.furry.y = this.furry.y - 1;
+      }
+      this.hideVisibleFurry()
+      this.showFurry()
+    }, 1050)
+  }
+
+  turnFurry = event => {
+    switch (event.which) {
+      case 37:
+        this.furry.direction = 'left';
+        break;
+      case 39:
+        this.furry.direction = 'right';
+        break;
+      case 38:
+        this.furry.direction = 'top';
+        break;
+      case 40:
+        this.furry.direction = 'down';
+        break;
     }
+  }
 }
 
 let startGame = new Game;
 startGame.showFurry();
 startGame.showCoin();
+startGame.startGame();
+
+document.addEventListener('keydown', function(event){
+  startGame.turnFurry(event);
+});
